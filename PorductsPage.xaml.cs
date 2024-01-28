24854ca591eefd1a76a6114ab17c39ddc9ef14f5
José Luis <joseluisgonzalezalamo@gmail.com>
2024-01-28 22:45:45 +0100

    commit

---
+++ PorductsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto2TrimestreInterfaces
+{
+    /// <summary>
+    /// Lógica de interacción para PorductsPage.xaml
+    /// </summary>
+    public partial class PorductsPage : Page
+    {
+        public PorductsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
